635eb030ff59622f27bd30588ef9bd015d96485c
Yuri Hallan V. De Souza <yurihallan9@gmail.com>
2019-11-16 20:27:01 -0400

    Primeiro Commit Com o projeto de CI.

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+	}
+
+
+	public function index()
+	{
+		$this->load->view('template/header');
+		$this->load->view('welcome_message');
+		$this->load->view('template/footer');
+	}
+
+
+}
